830d1fc15d370d7405469ad575b0daae2648ea84
kjjkjjzyayufqza <kjjkjjzyayufqza@yahoo.com>
2021-06-25 12:57:15 +0800

    updata

--- Change_Password_Profile_Staff.php
+++ Change_Password_Profile_Staff.php
@@ -13,14 +13,14 @@
 	<body>
 	<?php
 							require_once("conn.php");
-							$sql = "SELECT * from customer";
+							$sql = "SELECT * from staff";
 							$rs = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 							while($rc = mysqli_fetch_assoc($rs)){
 								$name = array("Name :", "Email :", "Phone :", "Address :", "Account Create Date :");
-								$array = array("customerName", "customerEmail", "phoneNumber", "address", "accountCreationDate");
+								$array = array("staffID");
 								printf('
 									<div class="container-fluid py-1 bg-secondary shadow-sm text-right text-white">
-									<i class="fa fa-user" aria-hidden="true"></i>  Customer - %s|
+									<i class="fa fa-user" aria-hidden="true"></i>  Staff - %s|
 									<a class="text-white font-weight-bold" href="index.html">Logout</a>',$rc[$array[0]]);
 							}
 
